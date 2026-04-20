b5b83391bc12e08079c87872766b4941842a2ff4
jhn02222 <jhn02222@uga.edu>
2026-04-19 23:46:48 -0400

    fix: add django.setup() to wsgi.py for Django 6 compatibility

--- athena/athena/wsgi.py
+++ athena/athena/wsgi.py
@@ -1,5 +1,7 @@
 import os
-from django.core.handlers.wsgi import WSGIHandler
+import django
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'athena.settings')
-application = WSGIHandler()
+django.setup()
+application = get_wsgi_application()
